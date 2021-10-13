bc299e8879685c22158b362d93fb7e7279edb181
Amal Almutairi <amalalmutairi001@gmail.com>
2021-10-13 11:02:34 +0300

    XML Format Assignment

---
+++ app/src/main/java/com/example/xml_format_assignment/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.xml_format_assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.rv)
+
+        var students: List<Students>? = null
+
+
+        try {
+            val parser = XmlPullParserHandler()
+            val istream = assets.open("studentdetails.xml")
+            students=  parser.parse(istream)
+
+            recyclerView.adapter = RVAdapter(students)
+            recyclerView.layoutManager = LinearLayoutManager(this)
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+}
